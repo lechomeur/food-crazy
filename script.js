6607b0d44dc9e388e55f3257ec6fe2b4ecac1470
lechomeur <madionrwa976@gmail.com>
2025-04-01 08:58:16 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Script pour activer le menu mobile
+document.getElementById('mobile-menu').addEventListener('click', function() {
+    document.getElementById('nav').classList.toggle('active');
+  });
+  
